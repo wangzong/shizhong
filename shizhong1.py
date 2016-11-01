de7a31f4e35822158bd3db3e381166009d09b16d
wangzong <wangzong@gmail.com>
2016-11-02 01:19:09 +0800

    fix bug in 1998.htm fix bug in 2000.htm fix bug in 2007.htm,2008.htm

--- shizhong1.py
+++ shizhong1.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import chardet
 
 
 def trim(string):
@@ -16,35 +17,51 @@ def trim(string):
 out_file = r"test.txt"
 writer = open(out_file, 'w', encoding='utf-8')
 
-for i in range(2000, 2016, 1):
+for i in range(1980, 2016, 1):
     url = 'http://past.nths.cn:8080/xiaoyou/xiaoyou/01/' + str(i) + ".htm"
     try:
         print(url)
         response = requests.get(url)
         page = response.content
-        unicode_page = page.decode('gbk')
+        charset = chardet.detect(page)
+        chartype = charset['encoding']
+
+        if chartype.find("UTF") >= 0:
+            unicode_page = page
+        else:
+            unicode_page = page.decode('gbk')
 
         soup = BeautifulSoup(unicode_page, "lxml")
 
-        fonts = soup.find_all(re.compile("font"))  # use tag:font to get the names
+        # fonts = soup.find_all(re.compile("font"))  # use tag:font to get the names
+        fonts = soup.find_all(name="font",attrs={"size":"2"})  # use tag:font with size=2 to get the names
 
         for font in fonts:
-            if font.string:
-                if font['color'] == '#FF6600' and font['size'] == '2':
-                    class_name = font.string
+            name = ""
+            for content in font.contents:
+                name = name + trim(content.string)
+            # print(name)
+            if name is not None:
+                # print(font.string)
+                # print(font.color)
+                if font.get('color') is not None:
+                    if font['color'] == '#FF6600':
+                        class_name = font.string
 
-                if font['color'] == '#0000FF':
-                    if font.string != "班主任":
-                        teacher_name = font.string
+                    if font['color'] == '#0000FF':
+                        if font.string != "班主任":
+                            teacher_name = name
 
-                if font['color'] == '#800000':
-                    student_name = font.string
-                    student_name = trim(student_name)
-                    print("%s,%s,%s,%s" % (str(i), class_name, teacher_name, student_name))
-                    writer.write("%s,%s,%s,%s\n" % (str(i), class_name, teacher_name, student_name))
+                    if font['color'] == '#800000' or font['color'] == '#990000':
+                        student_name = name
+                        student_name = trim(student_name)
+                        if student_name != "":
+                            print("%s,%s,%s,%s" % (str(i), class_name, teacher_name, student_name))
+                            writer.write("%s,%s,%s,%s\n" % (str(i), class_name, teacher_name, student_name))
 
 
     except Exception as e:
+        print("Exception")
         print(e)
 
 writer.close()
